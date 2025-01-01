cbcd302dcaaf659a01a801b9df927e3ebdd5403e
muzhawir <muzhawir.amri@gmail.com>
2025-01-01 14:33:56 +0800

    Merge branch 'development'

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: ['Inter', 'sans-serif'],
+			serif: ['serif'],
+			mono: ['JetBrains Mono', 'monospace'],
+		},
 	},
-	plugins: [],
+	plugins: [
+		require("@tailwindcss/typography"),
+		require('daisyui')
+	],
+	daisyui: {
+		themes: ["corporate", "business"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+	},
 }
