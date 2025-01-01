2e9285718e584159e778d8e2930336e23ffb6ed3
muzhawir <muzhawir.amri@gmail.com>
2025-01-01 17:00:26 +0800

    Add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,19 @@ import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: cloudflare()
+	site: "https://muzhawir.com",
+	integrations: [
+		tailwind(),
+		sitemap({
+			entryLimit: 45000,
+			changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date('2025-01-01'),
+		})
+	],
+	adapter: cloudflare()
 });
