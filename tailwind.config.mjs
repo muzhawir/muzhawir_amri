846b5f37b6981166f07011368d801e1aa070c00b
muzhawir <muzhawir.amri@gmail.com>
2025-01-01 14:03:05 +0800

    Add Font Family Configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,17 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: ['Inter', 'sans-serif'],
+			serif: ['serif'],
+			mono: ['JetBrains Mono', 'monospace'],
+		},
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
 		require('daisyui')
 	],
 	daisyui: {
-		themes: "corporate", // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "business", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-  },
+		themes: ["corporate", "business"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+	},
 }
